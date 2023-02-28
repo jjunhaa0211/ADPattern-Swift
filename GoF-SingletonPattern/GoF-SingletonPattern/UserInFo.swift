b474e958e945870b0a25de7ea3338dcc43c16dd8
junha <goodjunha@gmail.com>
2023-02-28 15:34:31 +0900

    [예제 주석] SingletonPattern 코드 설명 주석 추가

--- GoF-SingletonPattern/GoF-SingletonPattern/UserInFo.swift
+++ GoF-SingletonPattern/GoF-SingletonPattern/UserInFo.swift
@@ -8,11 +8,14 @@
 import Foundation
 
 class UserInFo {
+    //static let 프로퍼티가 추가
+    // let 으로 선언되었기 때문에 인스턴스 자체를 변경 할수도 없습니다.
     static let shared = UserInFo()
 
     var name: String?
     var password: String?
     var age: Int?
     
+    // Instance를 또 생생하는 것을 막기 위해 private 추가
     private init() { }
 }
