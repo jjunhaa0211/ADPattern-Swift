ac0dffdc9d5488192f51346969b67cd813981ecd
junha <goodjunha@gmail.com>
2023-08-11 15:34:44 +0900

    RIbs 예제 파일 추가

---
+++ RIBs/RIBsProject/RIBsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsProject
+//
+//  Created by 박준하 on 2023/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
