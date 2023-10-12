2a657910fd3294effeb4f0d339ea7fa3d01aed13
junha <goodjunha@gmail.com>
2023-10-12 10:54:40 +0900

    MVVM 예제 세팅

---
+++ MVVM/BasicMVVM/BasicMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicMVVM
+//
+//  Created by 박준하 on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
